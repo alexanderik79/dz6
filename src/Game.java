db94e183efb09fffa93b42a3bc5e35dada8b5187
alexanderik79 <alexanderik79@gmail.com>
2023-06-06 20:37:35 +0200

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Game {
+    public void play(){
+        String crossWord = randomWord();
+        // String wordCode = crossWord.subSequence(0, 2) + "###############";
+        String wordCode = "###############";
+        System.out.println("Вгадай слово: \n" + wordCode);
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String userWord = scanner.next();
+            if (userWord.equals(crossWord)) {
+                System.out.println("YOU WIN !!!");
+                break;
+            }
+            else {
+                System.out.println("Спробуй ще:");
+            }
+            wordCode = wordCodeMethod(crossWord,userWord,wordCode);
+            System.out.println(wordCode);
+        }
+    }
+    public String randomWord (){
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot",
+                "avocado" , "broccoli", "carrot", "cherry", "garlic", "grape", "melon",
+                "leak", "kiwi", "mango", "mushroom", "nut", "olive", " pea", "peanut",
+                "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        int word = (int)(Math.random() * words.length);
+        return words[word-1];
+    }
+    public String wordCodeMethod (String crossWord, String userWord, String wordCode){
+        String result = "";
+        if (crossWord.contains(userWord)) {
+            result = userWord + "###############";
+            return result;
+        }
+        else return wordCode;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        String s = "come on baby light my fire";
+        System.out.println(findSymbolOccurance(s, 'e'));
+        System.out.println(findWordPosition(s, "baby"));
+        System.out.println(stringReverse(s));
+        System.out.println(isPalindrome("Don't nod"));
+
+        Game game = new Game();
+        game.play();
+    }
+    public static int findSymbolOccurance (String string, char symbol ){
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if(symbol == string.charAt(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static int findWordPosition (String source, String target){
+        if (source.contains(target)){
+            return source.indexOf(target);
+        }
+        return -1;
+    }
+    public static String stringReverse (String string){
+        StringBuilder builder = new StringBuilder(string);
+        builder.reverse();
+        return String.valueOf(builder);
+    }
+    public static boolean isPalindrome (String palinString){
+        StringBuilder builder = new StringBuilder(palinString.toLowerCase().replaceAll("[^a-zа-я]", ""));
+        String str = String.valueOf(builder);
+        builder.reverse();
+        System.out.println(builder);
+        return String.valueOf(builder).equals(str);
+    }
+}
